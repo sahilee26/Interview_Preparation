5525f4bfe45d3d57739c8022cc38477496f0d436
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2020-08-24 00:13:14 +0530

    Update InterviewBit-Longest Valid Parenthesis.cpp

--- Solutions seen/InterviewBit-Longest Valid Parenthesis.cpp
+++ Solutions seen/InterviewBit-Longest Valid Parenthesis.cpp
@@ -3,14 +3,14 @@ int Solution::longestValidParentheses(string A) {
     int ans=0;
     stack<int> s;
     s.push(-1);
-    int valind=-1;
     for(int i=0; i<len; i++){
         if(A[i]=='('){
             s.push(i);
         }
         else{
-            s.pop();
-            if(!s.empty()){
+            int ind=s.top();
+            if(A[ind]=='('){
+                s.pop();
                 ans=max(ans, i-s.top());
             }
             else

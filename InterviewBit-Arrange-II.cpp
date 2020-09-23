e5e07bf6e55927f44b47fd943e7463410cf0db3b
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2020-09-23 17:36:50 +0530

    Update InterviewBit-Arrange-II.cpp

--- InterviewBit-Arrange-II.cpp
+++ InterviewBit-Arrange-II.cpp
@@ -1,9 +1,10 @@
-int rec(string A, vector<vector<int>>dp, int i, int n, int stable, int B){
+int dp[1005][1005];
+int rec(string A, int i, int n, int stable, int B){
     if(stable>B || i>=n)
-        return INT_MAX;
+        return 1e9;
     if(dp[i][stable-1]!=-1)
         return dp[i][stable-1];
-    int cost=INT_MAX;
+    int cost=1e9;
     int white=0, black=0;
     for(int j=i; j<n; j++){
         if(A[j]=='W')
@@ -11,7 +12,7 @@ int rec(string A, vector<vector<int>>dp, int i, int n, int stable, int B){
         else if(A[j]=='B')
             black++;
         if(j+1<n && stable<B)
-            cost=min(cost, black*white + rec(A, dp, j+1, n, stable+1, B));
+            cost=min(cost, black*white + rec(A, j+1, n, stable+1, B));
         else if(j==n-1 && stable==B)
             cost=min(cost, black*white);
     }
@@ -22,6 +23,8 @@ int rec(string A, vector<vector<int>>dp, int i, int n, int stable, int B){
 
 int Solution::arrange(string A, int B) {
     int n=A.length();
-    vector<vector<int> > dp (n,vector<int>(B, -1));
-    return rec(A, dp, 0, n,  1, B);
+    if(n<B)
+        return -1;
+    memset(dp, -1, sizeof(dp));
+    return rec(A, 0, n,  1, B);
 }

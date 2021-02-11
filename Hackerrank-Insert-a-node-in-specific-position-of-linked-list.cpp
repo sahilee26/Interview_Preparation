df86c849808d74b608f51d1d7bc8b9a645257367
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2021-02-11 23:48:32 +0530

    Create Hackerrank-Insert-a-node-in-specific-position-of-linked-list.cpp

---
+++ Hackerrank-Insert-a-node-in-specific-position-of-linked-list.cpp
@@ -0,0 +1,98 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the insertNodeAtPosition function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* insertNodeAtPosition(SinglyLinkedListNode* head, int data, int position) {
+    struct SinglyLinkedListNode* prev = NULL;
+    struct SinglyLinkedListNode* curr = head;
+    struct SinglyLinkedListNode* new1 = new SinglyLinkedListNode(data);
+    
+    new1->data=data;
+    
+    if(position==0){
+        new1->next=curr;
+        return new1;
+    }
+    int len=0;
+    while(curr!=NULL){
+        
+        if(len==position){
+            new1->next = curr;
+            prev->next = new1;
+            break;
+        }
+        prev=curr;
+        curr=curr->next;
+        len++;
+        
+    }
+    return head;
+}
+
+int main()

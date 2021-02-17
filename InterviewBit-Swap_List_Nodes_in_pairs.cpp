7b6a44f60825745885f18d05ed305f1ad40778dd
Sahil Aggarwal <31005698+sahilee26@users.noreply.github.com>
2021-02-17 21:20:04 +0530

    Create InterviewBit-Swap_List_Nodes_in_pairs.cpp

---
+++ InterviewBit-Swap_List_Nodes_in_pairs.cpp
@@ -0,0 +1,21 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ 
+ 
+ListNode* Solution::swapPairs(ListNode* A) {
+    ListNode* temp=A;
+    
+    while(temp!=NULL && temp->next!=NULL){
+       
+        swap(temp->val, temp->next->val);
+        
+        temp=temp->next->next;
+    }
+     return A;
+}
